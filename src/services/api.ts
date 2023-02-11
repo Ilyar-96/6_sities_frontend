c7f9907b71c624b70ac9eec4bf59d323d2f00514
Ilyar <kasyymov.ilyar@gmail.com>
2023-02-11 18:40:06 +0300

    fix url

--- src/services/api.ts
+++ src/services/api.ts
@@ -18,8 +18,8 @@ const StatusCodeMapping: Record<number, boolean> = {
 const shouldDisplayError = (response: AxiosResponse) =>
 	!!StatusCodeMapping[response.status];
 
-export const BACKEND_URL = "http://localhost:5000/";
-// export const BACKEND_URL = "https://six-sities.onrender.com/";
+// export const BACKEND_URL = "http://localhost:5000/";
+export const BACKEND_URL = "https://six-sities.onrender.com/";
 const REQUEST_TIMEOUT = 10000;
 
 export const createAPI = (): AxiosInstance => {
